3999d16a4e63721f9f38a8d7872fee4e8bc58b2a
talo <tal.oyar@spotinst.com>
2024-03-05 09:41:29 +0200

    [src] Updated rules

--- hackathon/hackathon_template/skills/get_group_details.py
+++ hackathon/hackathon_template/skills/get_group_details.py
@@ -2,10 +2,10 @@ def skill(account_id, group_id):
     import requests
 
     params = {
-        'groupId': 'sig-63c7dd3c'
+        'groupId': group_id
     }
 
-    url = 'http://core-public.dev.spotinst.com:2000/aws/ec2/group?spotinstAccountId=act-ef5a3926'
+    url = f'http://core-public.dev.spotinst.com:2000/aws/ec2/group?spotinstAccountId={account_id}'
     headers = {
         'Content-Type': 'application/json',
         'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzcG90aW5zdCIsImV4cCI6MTg5NDYxMjE5OSwiaWF0IjoxNTc5MjUyMTk5LCJ1aWQiOi0xLCJyb2xlIjoyLCJvaWQiOiI2MDYwNzk4NjU5MjEifQ.l5fjLRh3jsyzCDtlvUG3NozaDWEDjlRNH9jKoJU8hYc"
